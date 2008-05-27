a3c63c4ac18111b697c6b1493f144307973cf514
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2008-05-27 16:19:56 +0000

    SDP record v0.1

--- serviceHID.py
+++ serviceHID.py
@@ -18,8 +18,15 @@ xml = ' \
     <sequence>								  	\
        <uuid value="0x0011"/>					\
      </sequence>								\
-  </sequence>								    \
+ </sequence>								    \
  </attribute>                                  \
+ <attribute id="0x0006">						\
+    <sequence>									\
+        <uint16 value="0x656e"/>				\
+        <uint16 value="0x006a"/>				\
+        <uint16 value="0x0100"/>				\
+    </sequence>									\
+  </attribute>									\
  <attribute id="0x0009">						\
     <sequence>									\
       <sequence>								\
@@ -61,7 +68,44 @@ xml = ' \
    <uint8 value="0x016"/>                    \
   </attribute>									\
   <attribute id="0x0204">                       \
-    <boolean value="0x01"/>                    \
+   <boolean value="true"/>                    \
+  </attribute>									\
+   <attribute id="0x0205">                       \
+   <boolean value="true"/>                    \
+  </attribute>									\
+  <attribute id="0x0206">                       \
+    <sequence>                                  \
+   <sequence>                                \
+        <uint8 value="0x22"/>                  \
+        <text enconding="hex" value="05 01 09 05 a1 01 85 01 15 00 26 ff 00 75 08 95 0b 06 00 ff 09 01 81 00 85 01 95 0b 09 01 b1 00 85 02 95 0b 09 01 b1 00 85 03 95 0b 09 01 b1 00 85 04 95 0b 09 01 b1 00 85 05 95 0b 09 01 b1 00 85 06 95 0b 09 01 b1 00 c0 00" />		\
+      </sequence>                               \
+  </sequence>								    \
+  </attribute>                                  \
+  <attribute id="0x0207">                       \
+    <sequence>                                  \
+   <sequence>                                   \
+   		<uint16 value="0x0409" />               \
+   		<uint16 value="0x0100" />				\
+   </sequence>                                  \
+  </sequence>								    \
+  </attribute>                                  \
+  <attribute id="0x0208">                       \
+   <boolean value="false"/>                    \
+  </attribute>									\
+   <attribute id="0x0209">                       \
+   <boolean value="true"/>                    \
+ </attribute>									\
+  <attribute id="0x020a">                       \
+  <boolean value="true"/>                    \
+  </attribute>									\
+  <attribute id="0x020b">                       \
+  <uint16 value="0x0100"/>                    \
+  </attribute>									\
+  <attribute id="0x020d">                       \
+  <boolean value="true"/>                    \
+  </attribute>									\
+  <attribute id="0x020e">                       \
+   <boolean value="false"/>                    \
   </attribute>									\
 </record>                                       \
 '
