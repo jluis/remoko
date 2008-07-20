50cd392bf4e73e8a796835000e94904c632b3782
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2008-07-20 16:41:57 +0000

    Mouse events calibration

--- test_ui.py
+++ test_ui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import evas
+import evas.decorators
 import ecore
 import ecore.evas
 import edje
@@ -25,19 +26,92 @@ def mouse_position(self,x1,y1):
 class Screen(edje.Edje):
     def __init__(self, canvas, file):
         edje.Edje.__init__(self, canvas, file=file, group="layout")
-        self.x_init, self.y_init = canvas.pointer_canvas_xy
+        self.x_init, self.y_init = 0,0
+        self.mouse_down = False
         #self.container = Container(self.evas)
         #self.part_swallow("contents", self.container)
-
-    @edje.decorators.signal_callback("mouse,clicked,1", "*")
-    def cb_on_btn_clicked(self, emission, source):
-    	
-		if source == "bt_right":
+        
+	
+	#@edje.decorators.signal_callback("mouse,clicked,1", "*")
+	#def cb_on_btn_clicked(self,emisson, source):
+		
+		#if source == "bt_right":
 			
+			#connection.send_event("02:02:000:000:000")
+			
+		#elif source == "bt_left":
+			
+			#connection.send_event("02:01:000:000:000")
+			
+			
+		#elif source == "quit":
+			
+			#connection.send_event("quit")
+			#connection.terminate_connection
+			#ecore.main_loop_quit()
+	
+	#@edje.decorators.signal_callback("mouse,down,1", "*")
+	#def on_mouse_over(self, emission, source):
+		
+		#print "Down Down"
+				
+	#@edje.decorators.signal_callback("mouse,up,1", "*")
+	#def on_edje_signal_mouse_up_key(self, emission, source):
+		
+		#if self.mouse_down != None:
+			#self.mouse_down = False
+			#print "up up"
+    	
+	#@edje.decorators.signal_callback("mouse_area_pressed", "*")
+	#def on_edje_signal_mouse_down_key(self, emission, source):
+		#self.mouse_down = True
+		#print "cenas"
+		
+	#@edje.decorators.signal_callback("mouse,move", "*")
+	#def on_mouse_down(self, emission, source):
+		#self.mouse_down = True
+		#print "cenas2"
+
+    @edje.decorators.signal_callback("mouse,down,1", "*")
+    def on_mouse_down(self, emission, source):
+		
+		self.mouse_down = True
+
+    @edje.decorators.signal_callback("mouse,up,1", "*")
+    def on_mouse_up(self, emission, source):
+		
+		self.mouse_down = False
+		self.x_init, self.y_init = 0,0
+
+    @edje.decorators.signal_callback("mouse_over_area", "*")
+    def on_mouse_move(self, emission, source):
+
+		if self.mouse_down == True:
+			x,y = canvas.pointer_canvas_xy
+			x1,y1 = mouse_position(self,x,y)
+
+			print x1
+			print y1
+				
+			mov = "02:00:" + str(x1) + ":" + str(y1) + ":000"
+			print mov
+			connection.send_event(mov)
+
+		else:
+
+			print "333"
+	
+    @edje.decorators.signal_callback("mouse,clicked,1", "*")
+    def on_mouse_over(self, emission, source):
+    	
+		print self.mouse_down
+		if source == "bt_right":
+			print "bt"
 			connection.send_event("02:02:000:000:000")
 			
 		elif source == "bt_left":
 			
+			print "bt_l"
 			connection.send_event("02:01:000:000:000")
 			
 			
@@ -48,18 +122,9 @@ class Screen(edje.Edje):
 			ecore.main_loop_quit()
 				
 		else:
-			
-			x,y = canvas.pointer_canvas_xy
-			x1,y1 = mouse_position(self,x,y)
-			
-			print x1
-			print y1
 				
-			mov = "02:00:" + str(x1) + ":" + str(y1) + ":000"
-			print mov
-			connection.send_event(mov)
-			
-		
+			pass
+												
 connection = Connect()
 while connection.connect == False:
 	
