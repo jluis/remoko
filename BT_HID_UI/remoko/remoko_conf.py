f86787d92443a60b88082c33e0402eed9bf44a8d
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2008-08-14 14:43:31 +0000

    presentation profile configurations completed

--- BT_HID_UI/remoko/remoko_conf.py
+++ BT_HID_UI/remoko/remoko_conf.py
@@ -11,14 +11,34 @@ class remoko_conf:
 		try:
 			
 			self.config.readfp(open('settings.cfg'))
-			self.fullscreen = self.config.get("user","fullscreen")
-			self.scroll = self.config.get("user","scroll")
-			
+			try:
+				#settings
+				self.fullscreen = self.config.get("user","fullscreen")
+				self.scroll = self.config.get("user","scroll")
+				#presentation profile
+				self.previous_key = self.config.get("presentation","previous_key")
+				self.next_key = self.config.get("presentation","next_key")
+				self.fullscreen_key = self.config.get("presentation","fullscreen_key")
+				self.no_fullscreen_key = self.config.get("presentation","no_fullscreen_key")
+			except:
+				print "ERROR: \'settings.ctg\' is damaged"
+				#put some error, to show in the UI
+				print "Error: Non such file or directory \'settings.cfg\'"
+				self.fullscreen = "Yes"
+				self.scroll = 0
+				self.previous_key = "Prior"
+				self.next_key = "Next"
+				self.fullscreen_key = "f5"
+				self.no_fullscreen_key = "Escape"
 		except:
 			#put some error, to show in the UI
-			print "Error: Non such file or directory"
+			print "Error: Non such file or directory \'settings.cfg\'"
 			self.fullscreen = "Yes"
 			self.scroll = 0
+			self.previous_key = "Prior"
+			self.next_key = "Next"
+			self.fullscreen_key = "f5"
+			self.no_fullscreen_key = "Escape"
 
 	def set_option(self,seccion,opt,value):
 		
@@ -36,7 +56,7 @@ class remoko_conf:
 			
 		except:
 			
-			print "Error: Non such file or directory"
+			print "Error: Non such file or directory \'settings.cfg\'"
 
 		
 		
