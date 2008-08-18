600524945e6a199140de8090a2d92e48941d5802
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2008-08-18 18:31:47 +0000

    Multimedia profile completed, added fake win key

--- trunk/BT_HID_UI/remoko/remoko_conf.py
+++ trunk/BT_HID_UI/remoko/remoko_conf.py
@@ -20,6 +20,17 @@ class remoko_conf:
 				self.next_key = self.config.get("presentation","next_key")
 				self.fullscreen_key = self.config.get("presentation","fullscreen_key")
 				self.no_fullscreen_key = self.config.get("presentation","no_fullscreen_key")
+				#multimedia profile
+				self.play_key = self.config.get("multimedia","play_key")
+				self.pause_key = self.config.get("multimedia","pause_key")
+				self.stop_key = self.config.get("multimedia","stop_key")
+				self.forward_key = self.config.get("multimedia","forward_key")
+				self.backward_key = self.config.get("multimedia","backward_key")
+				self.fullscreen_key_m = self.config.get("multimedia","fullscreen_key_m")
+				self.volume_m_key = self.config.get("multimedia","volume_m_key")
+				self.volume_p_key = self.config.get("multimedia","volume_p_key")
+				self.no_fullscreen_key_m = self.config.get("multimedia", "no_fullscreen_key_m")
+				
 			except:
 				print "ERROR: \'settings.ctg\' is damaged"
 				#put some error, to show in the UI
@@ -30,6 +41,16 @@ class remoko_conf:
 				self.next_key = "Next"
 				self.fullscreen_key = "f5"
 				self.no_fullscreen_key = "Escape"
+				#multimedia profile
+				self.play_key = "p"
+				self.pause_key = "p"
+				self.stop_key = "s"
+				self.forward_key = "right"
+				self.backward_key = "left"
+				self.fullscreen_key_m = "f"
+				self.volume_m_key = "minus"
+				self.volume_p_key = "plus"
+				self.no_fullscreen_key_m = "Escape"
 		except:
 			#put some error, to show in the UI
 			print "Error: Non such file or directory \'settings.cfg\'"
@@ -39,6 +60,16 @@ class remoko_conf:
 			self.next_key = "Next"
 			self.fullscreen_key = "f5"
 			self.no_fullscreen_key = "Escape"
+			#multimedia profile
+			self.play_key = "p"
+			self.pause_key = "p"
+			self.stop_key = "s"
+			self.forward_key = "right"
+			self.backward_key = "left"
+			self.fullscreen_key_m = "f"
+			self.volume_m_key = "minus"
+			self.volume_p_key = "plus"
+			self.no_fullscreen_key_m = "Escape"
 
 	def set_option(self,seccion,opt,value):
 		
@@ -60,3 +91,8 @@ class remoko_conf:
 
 		
 		
+
+
+#cenas = remoko_conf()
+#print cenas.fullscreen
+#cenas.set_option("user", "fullscreen", "No")
