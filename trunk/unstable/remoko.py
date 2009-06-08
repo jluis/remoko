5ddc366265cdc15710337fbd715b6bd27ab01eb5
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2009-06-08 21:18:25 +0000

    deleted old app structure-2

--- trunk/unstable/remoko.py
+++ trunk/unstable/remoko.py
@@ -2,7 +2,7 @@
 #
 #      remoko.py
 #
-#      Copyright 2008 	Valerio Valerio <vdv100@gmail.com>
+#      Copyright 2008 -2009 Valerio Valerio <vdv100@gmail.com>
 #						
 #
 #      This program is free software; you can redistribute it and/or modify
