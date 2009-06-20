798ee008bdeed1b574e526e0ff7b75ec73d99c7a
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2009-06-20 11:04:57 +0000

    code refactoring - disconnect class

--- unstable/remoko.py
+++ unstable/remoko.py
@@ -42,6 +42,7 @@ from remoko_key_mapper import *
 from remoko_conf import *
 from remoko_edje_group import *
 from remoko_mouse import *
+from remoko_disconnect import *
 
 WIDTH = 480
 HEIGHT = 640
@@ -135,36 +136,7 @@ class main(edje_group):
 			ecore.idle_enterer_add( self.main.check_connection_status)
 			ecore.timer_add(3.0,self.main.transition_to,"menu")
 
-#----------------------------------------------------------------------------#
-class disconnect(edje_group):
-#----------------------------------------------------------------------------#
-    def __init__(self, main):
-        edje_group.__init__(self, main, "disconnect")
-        
-	self.part_text_set("label_error","Error: Disconnected by remote device")
-	self.part_text_set("label_connect", "Open connection again ?")
-    @edje.decorators.signal_callback("mouse,clicked,1", "*")
-    def on_edje_signal_button_pressed(self, emission, source):
-	if source == "quit" or source == "no_option" :
-		
-		self.main.connection.terminate_connection()
-		if self.main.connection.connect == False:
-			os.system("pkill  -9 hidclient")
-		self.main.on_exit()
-		ecore.main_loop_quit()
 
-	if source == "yes_option":
-
-		self.main.connection.terminate_connection()
-		if self.main.connection.connect == False:
-			os.system("pkill  -9 hidclient")
-		self.main.initialize_remoko_server()
-		self.main.groups["main"].part_text_set("label_connect_to", "")
-		self.main.groups["main"].part_text_set("label_client", "")
-		self.main.groups["main"].part_text_set("label_waiting", "Waiting for connection ... ")
-		ecore.timer_add(1.0,self.main.groups["main"].check_client)
-		self.main.transition_to("main")
-		
 #----------------------------------------------------------------------------#
 class connection_status(edje_group):
 #----------------------------------------------------------------------------#
