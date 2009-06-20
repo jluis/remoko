e52106b40666c8d88893f7d937949acd1ba22dee
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2009-06-20 11:06:54 +0000

    code refactoring - disconnect class-2

---
+++ unstable/remoko_disconnect.py
@@ -0,0 +1,62 @@
+#
+#      remoko_disconnect.py
+#
+#      Copyright 2008 -2009 	Valerio Valerio <vdv100@gmail.com>
+#						
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+
+
+import e_dbus
+import evas
+import evas.decorators
+import edje
+import edje.decorators
+import ecore
+import ecore.x
+import ecore.evas
+from remoko_edje_group import *
+
+#----------------------------------------------------------------------------#
+class disconnect(edje_group):
+#----------------------------------------------------------------------------#
+    def __init__(self, main):
+        edje_group.__init__(self, main, "disconnect")
+        
+	self.part_text_set("label_error","Error: Disconnected by remote device")
+	self.part_text_set("label_connect", "Open connection again ?")
+
+    def onShow( self ):
+	self.focus = True
+    
+
+    def onHide( self ):
+	self.focus = False
+
+    @edje.decorators.signal_callback("mouse,clicked,1", "*")
+    def on_edje_signal_button_pressed(self, emission, source):
+	if source == "quit" or source == "no_option" :
+		
+		self.main.connection.terminate_connection()
+		self.main.on_exit()
+		ecore.main_loop_quit()
+
+	if source == "yes_option":
+	
+		self.main.connection.terminate_connection()
+		self.main.transition_to("process_conn")
+		
