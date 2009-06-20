0d9bb42162ec37899039903072600efba865a849
vdv100 <vdv100@9289b43e-364e-0410-83bf-ff67881ba18e>
2009-06-20 14:15:56 +0000

    code refactoring - menu and presentation classes-2

---
+++ trunk/unstable/remoko_menu.py
@@ -0,0 +1,114 @@
+#
+#      remoko_menu.py
+#
+#      Copyright 2008 -2009 	Valerio Valerio <vdv100@gmail.com>
+#						
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+
+
+import e_dbus
+import evas
+import evas.decorators
+import edje
+import edje.decorators
+import ecore
+import ecore.x
+import ecore.evas
+from remoko_edje_group import *
+
+
+#----------------------------------------------------------------------------#
+class menu(edje_group):
+#----------------------------------------------------------------------------#
+    def __init__(self, main):
+        edje_group.__init__(self, main, "menu")
+	self.part_text_set( "label_title", "ReMoko" )
+
+    def onShow( self ):
+	self.focus = True
+    
+
+    def onHide( self ):
+	self.focus = False
+        
+    @edje.decorators.signal_callback("mouse,clicked,1", "*")
+    def on_edje_signal_button_pressed(self, emission, source):
+ 
+		if source == "quit":
+		
+			self.main.connection.terminate_connection()
+			self.main.on_exit()
+			ecore.main_loop_quit()
+			
+		elif source == "mouse":
+			
+			self.main.transition_to("mouse_ui")
+
+		elif source == "keyboard":
+			
+			self.main.transition_to("keyboard_ui")
+
+		elif source == "presentation":
+			
+			self.main.transition_to("presentation")
+
+		elif source == "multimedia":
+			
+			self.main.transition_to("multimedia")
+
+		elif source == "games":
+			
+			self.main.transition_to("games")
+
+		elif source == "accelerometer":
+			self.main.accelerometer_prev = "menu"
+			self.main.transition_to("accelerometer")
+
+		elif source == "connection":
+
+			if self.main.connection.connect == True:
+				self.main.groups["connection_status"].part_text_set("label_connect_to","Connected to:")
+    				self.main.groups["connection_status"].part_text_set("label_client", self.main.connection.client_name)
+				self.main.groups["connection_status"].part_text_set("label_addr",self.main.connection.client_addr)
+			else:
+				self.part_text_set("label_not_connect","You are not connect to any device")
+		
+			self.main.transition_to("connection_status")
+
+		elif source == "about":
+			
+			self.main.groups["about"].part_text_set("label_version","ReMoko v0.4")
+			self.main.groups["about"].part_text_set("label_developed","Developed by:")
+			self.main.groups["about"].part_text_set("label_name","Valerio Valerio")
+			self.main.groups["about"].part_text_set("label_email","<vdv100@gmail.com>")
+			self.main.groups["about"].part_text_set("label_thanks","Thanks to:")
+			self.main.groups["about"].part_text_set("label_claudio","Claudio Takahasi")
+			self.main.groups["about"].part_text_set("label_daniel","Daniel Willmann ")
+			self.main.groups["about"].part_text_set("label_joachim","Joachim Breitner")
+			
+			
+			self.main.transition_to("about")	
+
+		elif source == "conf":
+
+			self.main.transition_to("settings")			
+		else:
+			
+			print "feature not implemented yet :) "
+
+
